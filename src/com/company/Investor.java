48d9ceef4ef886ac1427538d5f20807cb3668200
AndreevM <andreevm@bk.ru>
2015-12-24 12:43:57 +0000

    Initial commit

---
+++ src/com/company/Investor.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Date;
+
+/**
+ * Created by myandreev on 23/12/2015.
+ */
+public class Investor {
+    public Investor(Date  _date)
+    {onboarding_date = _date;}
+
+    protected
+        Date onboarding_date;
+}
